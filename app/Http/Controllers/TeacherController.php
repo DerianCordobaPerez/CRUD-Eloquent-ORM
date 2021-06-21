7da5d849d2f2d3b62be1fa521559b3db677a07be
DerianCordobaPerez <derianunanleon@gmail.com>
2021-06-21 11:44:17 -0600

    controlador teacher terminado

--- app/Http/Controllers/TeacherController.php
+++ app/Http/Controllers/TeacherController.php
@@ -4,12 +4,13 @@ namespace App\Http\Controllers;
 
 use App\Models\Teacher;
 use Illuminate\Contracts\Support\Renderable;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
-use Illuminate\Http\Response;
-use Illuminate\Routing\Redirector;
 
-class TeacherController extends Controller
-{
+
+class TeacherController extends Controller {
+    const ROUTE = 'http://127.0.0.1:8000/';
+
     /**
      * Display a listing of the resource.
      *
@@ -32,11 +33,11 @@ class TeacherController extends Controller
      * Store a newly created resource in storage.
      *
      * @param Request $request
-     * @return Redirector|Response
+     * @return RedirectResponse
      */
-    public function store(Request $request):Redirector|Response {
+    public function store(Request $request): RedirectResponse {
         (new Teacher())->create($request->all());
-        return redirect('/teachers/show')->with('message', 'Se agregado correctamente el profesor')->with('teachers', Teacher::all());
+        return redirect()->away(self::ROUTE.'teacher/show')->with('message', 'Se agregado correctamente el profesor')->with('teachers', Teacher::all());
     }
 
     /**
@@ -52,35 +53,36 @@ class TeacherController extends Controller
      * Show the form for editing the specified resource.
      *
      * @param $id
-     * @return Renderable
+     * @return RedirectResponse
      */
-    public function edit($id): Renderable {
-        return view('teachers.createOrEdit')->with('message')->with('teacher', (new Teacher())->find($id));
+    public function edit($id): RedirectResponse {
+        return redirect()->away(self::ROUTE.'teacher/show')->with('message')->with('teacher', (new Teacher())->find($id));
     }
 
     /**
      * Update the specified resource in storage.
      *
      * @param Request $request
-     * @return mixed
+     * @return RedirectResponse
      */
-    public function update(Request $request): mixed {
+    public function update(Request $request): RedirectResponse
+    {
         (new Teacher())->where('id', '=', $request->id ?? '')->update([
             'name' => $request->name ?? '',
             'lastName' => $request->lastName ?? '',
             'title' => $request->title ?? ''
         ]);
-        return redirect('/teacher/show')->with('message', 'Profesor actualizado correctamente')->with('teachers', Teacher::all());
+        return redirect()->away(self::ROUTE.'teacher/show')->with('message', 'Profesor actualizado correctamente')->with('teachers', Teacher::all());
     }
 
     /**
      * Remove the specified resource from storage.
      *
      * @param $id
-     * @return Redirector
+     * @return RedirectResponse
      */
-    public function destroy($id):Redirector {
+    public function destroy($id): RedirectResponse {
         (new Teacher())->find($id)->delete();
-        return redirect('/');
+        return redirect()->away(self::ROUTE);
     }
 }
